c491bf10a58d540a1c8d5ffe6ad6826c72b8e75a
Dalius Juocas <dalius.juocas@gmail.com>
2024-06-11 14:57:17 +0300

    fix: gifs table migration

--- src/database/migrations/20240611100810-updateGifsTable.ts
+++ src/database/migrations/20240611100810-updateGifsTable.ts
@@ -1,10 +1,7 @@
 import { Kysely, SqliteDatabase, sql } from 'kysely'
 
 export async function up(db: Kysely<SqliteDatabase>) {
-  await db.schema
-    .alterTable('gifs')
-    .addColumn('url_backup', 'text', (c) => c.notNull())
-    .execute()
+  await db.schema.alterTable('gifs').addColumn('url_backup', 'text').execute()
   await sql`UPDATE gifs SET url_backup = url`.execute(db)
   await db.schema.alterTable('gifs').renameColumn('url', 'url_unused').execute()
   await db.schema.alterTable('gifs').renameColumn('url_backup', 'url').execute()
