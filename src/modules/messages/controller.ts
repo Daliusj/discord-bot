ba8ccda02f5620b3e0f4a4245862dcdd0d71d6f6
Dalius Juocas <dalius.juocas@gmail.com>
2024-06-13 11:15:40 +0300

    fix: messages module controller

--- src/modules/messages/controller.ts
+++ src/modules/messages/controller.ts
@@ -24,14 +24,32 @@ export default (db: Database, discord: Discord, giphy: Giphy) => {
     .get(
       '/',
       jsonRoute(async (req) => {
-        if (req.query.username)
-          return services.getAllMessagesByUserName(
-            parseName(req.query.username)
+        const { username, sprint } = req.query
+
+        if (username && sprint) {
+          const userMessages = await services.getAllMessagesByUserName(
+            parseName(username)
           )
-        if (req.query.sprint)
-          return services.getAllMessagesBySprintCode(
-            parseSprintsCode(req.query.sprint)
+          const sprintMessages = await services.getAllMessagesBySprintCode(
+            parseSprintsCode(sprint)
           )
+          const combinedMessages = userMessages.filter((message) =>
+            sprintMessages.some(
+              (sprintMessage) =>
+                sprintMessage.sprintsCode === message.sprintsCode
+            )
+          )
+          return combinedMessages
+        }
+
+        if (username) {
+          return services.getAllMessagesByUserName(parseName(username))
+        }
+
+        if (sprint) {
+          return services.getAllMessagesBySprintCode(parseSprintsCode(sprint))
+        }
+
         return services.getAllMessages()
       })
     )
